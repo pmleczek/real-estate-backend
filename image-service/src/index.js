be8507f4f9eecebcd424affade4b8d691eea97f5
hyper <patrykmleczek.pm@gmail.com>
2023-04-20 23:32:18 +0200

    Add MongoDB to image-service

--- image-service/src/index.js
+++ image-service/src/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 app = express();
 
+const { MongoClient } = require("mongodb")
+const dbURL = process.env.DB_URL;
+const dbName = process.env.DB_NAME;
+const mongoClient = new MongoClient(dbURL);
+
+const connect = async () => {
+    try {
+        await mongoClient.connect();
+        console.log("Successfully connected to MongoDB server");
+        const db = mongoClient.db(dbName);
+        app.set("db", db);
+    } catch (err) {
+        console.log("Error connecting to MongoDB server");
+        console.log(err.message);
+    }
+}
+await connect();
+
 const port = 8080;
 const server = app.listen(port, () => {
     console.log(`App listening on port ${port}`);
