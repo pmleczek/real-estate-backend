e384afb03bac3ee27713e2d738ab3b31ca4e8f2b
hyper <patrykmleczek.pm@gmail.com>
2023-04-26 14:43:33 +0200

    Initialize location-service project

---
+++ location-service/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const port = 8080;
+const server = app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM received");
+    console.log("Closing HTTP server...");
+    server.close(() => {
+
+        console.log("HTTP server closed");
+    });
+});
