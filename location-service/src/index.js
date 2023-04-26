5516777bb7c84ab1c827ccb0fe8590a6c04af449
hyper <patrykmleczek.pm@gmail.com>
2023-04-26 18:45:27 +0200

    Add Apollo Server to location-service

--- location-service/src/index.js
+++ location-service/src/index.js
@@ -1,6 +1,20 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import express from "express";
 
 const app = express();
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+await apolloServer.start();
+
+app.use(
+    "/graphql",
+    express.json(),
+    expressMiddleware(apolloServer),
+);
 
 const port = 8080;
 const server = app.listen(port, () => {
@@ -11,7 +25,6 @@ process.on("SIGTERM", () => {
     console.log("SIGTERM received");
     console.log("Closing HTTP server...");
     server.close(() => {
-
         console.log("HTTP server closed");
     });
 });
