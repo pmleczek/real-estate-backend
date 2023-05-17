13adbd2fac193e3b34e1945ae9b1d151a0c01e79
hyper <patrykmleczek.pm@gmail.com>
2023-05-18 00:14:52 +0200

    Add listing-service database initialization to initdb

--- initdb/createIndex.js
+++ initdb/createIndex.js
@@ -1,2 +1,5 @@
-db = connect( 'mongodb://node:node@locationdb:27017/location?authSource=admin' );
+db = connect('mongodb://node:node@locationdb:27017/location?authSource=admin');
 db.locations.createIndex({ name: "text" })
+
+db = connect('mongodb://node:node@listingdb:27017/listing?authSource=admin');
+db.listings.createIndex({ location: "2dsphere" })
--- initdb/initmongo.sh
+++ initdb/initmongo.sh
@@ -3,3 +3,7 @@ echo "Initializing location-service database"
 mongoimport --host locationdb --db location --collection locations --type json /initdb/locations.json --jsonArray --username node\
  --password node --authenticationDatabase admin
 mongosh --host locationdb --db location --username node --password node --authenticationDatabase admin --file /initdb/createIndex.js
+
+echo "Initializing listing-service database"
+mongoimport --host listingdb --db listing --collection listings --type json /initdb/listings.json --jsonArray --username node\
+ --password node --authenticationDatabase admin
